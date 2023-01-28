ddaf5f39536344010d3f43c7a70f5c5bbef1e03f
Manav39 <Manav39>
2023-01-28 23:46:51 +0530

    sidebar-changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Features } from './components/Features';
 import {BrowserRouter , Routes ,Route} from 'react-router-dom';
-import {Sidebar} from './components/Sidebar';
 import {Login} from './components/Login'
-import {Side} from './components/Side';
+import {Dashboard} from './components/Dashboard'
+import {Sidebar} from './components/Sidebar'
 function App() {
   return (
    	<>
 	<BrowserRouter>
-	<Side />
 	<Routes>
 		<Route path='/' element={<Features/>}/>
 		<Route path='/login' element={<Login/>}/>
+		<Route path='/dashboard' element={<Dashboard/>}/>
 	</Routes>
 	</BrowserRouter>
 	</>
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Grid,Paper, TextField, Button } from "@mui/material";
+import LockIcon from '@mui/icons-material/Lock';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PasswordIcon from '@mui/icons-material/Password';
+import BusinessIcon from '@mui/icons-material/Business';
 
-const Login = () => {
+
+function Login(){
+  const paperStyle = {padding: 20, maxWidth: 400, margin: "100px auto"}
+  const avatarStyle = {backgroundColor: "#1976d2"}
+  const iconStyle = {backgroundColor: "#1976d2",marginTop:'10px'} 
   return (
-    <div>Login</div>
+    <>
+      <Grid sx={{fontFamily:"sans-serif", margin: "25px"}}>
+        <Paper elevation={10} style={paperStyle}>
+            <Grid>
+                <center>
+                  <Avatar style={avatarStyle}><LockIcon></LockIcon></Avatar>
+                  <h2>Login</h2>
+                </center>
+            </Grid>
+            <div style={{display:'flex',gap:'0.5rem'}}>
+            <Avatar style={iconStyle}><BusinessIcon fontSize='small'></BusinessIcon></Avatar><TextField id="outlined-basic" label="Organization Id" variant="outlined" fullWidth required/> 
+            </div>
+            
+            <br></br>
+            <div style={{display:'flex',gap:'0.5rem'}}>
+            <Avatar style={iconStyle}><AccountCircleIcon fontSize='small'></AccountCircleIcon></Avatar><TextField id="outlined-basic" label="Username" variant="outlined" fullWidth required/> 
+            </div>
+            
+            <br></br>
+            <div style={{display:'flex',gap:'0.5rem'}}>
+            <Avatar style={iconStyle}><PasswordIcon fontSize='small'></PasswordIcon></Avatar><TextField id="outlined-basic" label="Password" type="password" variant="outlined" fullWidth required/>
+            </div>
+            
+            <br></br>
+            <br></br>
+            <center><Button variant="contained" fullWidth>Login</Button></center>
+        </Paper>
+    </Grid>
+    </>
   )
 }
 
